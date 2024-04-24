97ac58eb6e79042a50e2818e6f62ce43e911ee8f
cprentic <cprentic@umd.edu>
2024-04-24 13:24:22 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography } from "antd";
+
+const { Title, Text } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title level={2}>My Title</Title>
+      <Text>Some text</Text>
     </div>
   );
 }
