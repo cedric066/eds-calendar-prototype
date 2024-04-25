52257530344ff2b702ea8ac1cefd0969dd723a59
cprentic <cprentic@umd.edu>
2024-04-25 09:47:27 -0400

    Calendar renders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,117 @@
+// WARNING: THIS IS A PROTOTYPE - SOME FUNCTIONALITY MAY BE MISSING
 import React from "react";
-import { Typography } from "antd";
+import { Badge, Calendar, Typography } from "antd";
 
-const { Title, Text } = Typography;
+// Get the text components we need from Typography
+const { Title } = Typography;
 
+// Get an event list for a certain day (when in month view)
+const getListData = (value) => {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event.',
+        },
+        {
+          type: 'success',
+          content: 'This is usual event.',
+        },
+      ];
+      break;
+    case 10:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event.',
+        },
+        {
+          type: 'success',
+          content: 'This is usual event.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event.',
+        },
+      ];
+      break;
+    case 15:
+      listData = [
+        {
+          type: 'warning',
+          content: 'This is warning event',
+        },
+        {
+          type: 'success',
+          content: 'This is very long usual event......',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 1.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 2.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 3.',
+        },
+        {
+          type: 'error',
+          content: 'This is error event 4.',
+        },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+};
+
+// Get notes for a certain month (when in year view)
+const getMonthData = (value) => {
+  if (value.month() === 8) {
+    return 1394;
+  }
+};
+
+// Render the calendar
 function App() {
+  const monthCellRender = (value) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const cellRender = (current, info) => {
+    if (info.type === 'date') return dateCellRender(current);
+    if (info.type === 'month') return monthCellRender(current);
+    return info.originNode;
+  };
+
   return (
     <div className="App">
-      <Title level={2}>My Title</Title>
-      <Text>Some text</Text>
+      <Title>Calendar Prototype</Title>
+      <Calendar cellRender={cellRender} />
     </div>
   );
 }
