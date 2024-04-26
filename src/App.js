1676c4e41a819eaa74f6b43dd303fbe0727433ea
cprentic <cprentic@umd.edu>
2024-04-26 12:49:20 -0400

    Swapped antd for react-big-calendar

--- src/App.js
+++ src/App.js
@@ -1,117 +1,27 @@
 // WARNING: THIS IS A PROTOTYPE - SOME FUNCTIONALITY MAY BE MISSING
 import React from "react";
-import { Badge, Calendar, Typography } from "antd";
+import events from "./events";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-// Get the text components we need from Typography
-const { Title } = Typography;
-
-// Get an event list for a certain day (when in month view)
-const getListData = (value) => {
-  let listData;
-  switch (value.date()) {
-    case 8:
-      listData = [
-        {
-          type: 'warning',
-          content: 'This is warning event.',
-        },
-        {
-          type: 'success',
-          content: 'This is usual event.',
-        },
-      ];
-      break;
-    case 10:
-      listData = [
-        {
-          type: 'warning',
-          content: 'This is warning event.',
-        },
-        {
-          type: 'success',
-          content: 'This is usual event.',
-        },
-        {
-          type: 'error',
-          content: 'This is error event.',
-        },
-      ];
-      break;
-    case 15:
-      listData = [
-        {
-          type: 'warning',
-          content: 'This is warning event',
-        },
-        {
-          type: 'success',
-          content: 'This is very long usual event......',
-        },
-        {
-          type: 'error',
-          content: 'This is error event 1.',
-        },
-        {
-          type: 'error',
-          content: 'This is error event 2.',
-        },
-        {
-          type: 'error',
-          content: 'This is error event 3.',
-        },
-        {
-          type: 'error',
-          content: 'This is error event 4.',
-        },
-      ];
-      break;
-    default:
-  }
-  return listData || [];
-};
-
-// Get notes for a certain month (when in year view)
-const getMonthData = (value) => {
-  if (value.month() === 8) {
-    return 1394;
-  }
-};
+// Set the locale for the calendar
+const localizer = momentLocalizer(moment);
 
 // Render the calendar
 function App() {
-  const monthCellRender = (value) => {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
-  };
-
-  const dateCellRender = (value) => {
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} />
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
-  const cellRender = (current, info) => {
-    if (info.type === 'date') return dateCellRender(current);
-    if (info.type === 'month') return monthCellRender(current);
-    return info.originNode;
-  };
-
   return (
     <div className="App">
-      <Title>Calendar Prototype</Title>
-      <Calendar cellRender={cellRender} />
+      <h1>Calendar Prototype</h1>
+      <Calendar
+        defaultDate={new Date(2024, 3, 1)}
+        events={events}
+        localizer={localizer}
+        onShowMore={(events, date) => this.setState({ showModal: true, events })}
+        popup={false}
+        step={60}
+        style={{ height: 700 }}
+      />
     </div>
   );
 }
